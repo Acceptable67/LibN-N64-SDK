2429013ca3f6a35f64be22af8656922a38ebba1a
Acceptable67 <31579132+Acceptable67@users.noreply.github.com>
2024-01-02 13:11:28 -0500

    revised function names

--- src/vector.cpp
+++ src/vector.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <cstring>
 
-void LibVector::Pushback(void *item) 
+void LibVector::push_back(void *item) 
 {
 	s32 vec_pos = this->pos;
 
@@ -13,25 +13,25 @@ void LibVector::Pushback(void *item)
 	++this->pos;
 }
 
-s32 LibVector::Size() {
+s32 LibVector::size() {
     return this->pos;
 }
 
-void* LibVector::At(s32 position) {
+void* LibVector::at(s32 position) {
 	if(position <= this->pos)
         return this->element[position];
 
     return NULL;
 }
 
-void LibVector::EraseAt(s32 position) {
+void LibVector::erase_at(s32 position) {
     u32 vec_size = sizeof(this->element[position]);
     if(vec_size >= 1) {
         memset(&this->element[position], 0, vec_size);
     }
 }
 
-void LibVector::Clear() {
+void LibVector::clear() {
     for(s32 index = 0; index <= this->pos; ++index) {
         memset(&this->element[index], 0, sizeof(this->element[index]));
     }
