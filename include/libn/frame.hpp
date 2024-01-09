397f7aee2d96c13eb98ef1db1ebb5fbc35748278
Spencer Chase <31579132+Acceptable67@users.noreply.github.com>
2024-01-09 12:13:25 -0500

    added C-Pad functions + redone Joystick w/ gamma option

--- include/libn/frame.hpp
+++ include/libn/frame.hpp
@@ -5,44 +5,55 @@
 #include <libn/controller.hpp>
 #include <libn/vi.hpp>
 
-namespace LibN64 {
-class Frame {
-  private:
-    Display::Resolution r;
-    Display::Bitdepth bd;
-    Display::AntiAliasing aa;
-    Display::Gamma g;
+namespace LibN64  
+{
+  class Frame
+  {
+    private:
+      Display::Resolution r;
+      Display::Bitdepth bd;
+      Display::AntiAliasing aa;
+      Display::Gamma g;
+      Display::TextColor tc;
 
-    bool bRunning     = false;
-    bool bClearScreen = false;
+      bool bRunning     = false;
+      bool bClearScreen = false;
 
-  public:
-    Frame(const Display::Resolution res, const Display::Bitdepth bitdepth,
-        const Display::AntiAliasing antialiasing);
-    Frame() = default;
+    public:
+      Frame(const Display::Resolution res, 
+            const Display::Bitdepth bitdepth, 
+            const Display::AntiAliasing antialiasing,
+            const Display::Gamma gamma, 
+            const Display::TextColor textcolor);
+      Frame() = default;
 
-    void Begin();
- 
-    virtual void FrameUpdate();
-    virtual void OnCreate();
-    virtual void KeyAPressed();
-    virtual void KeyBPressed();
-    virtual void KeyZPressed();
-    virtual void KeyDUpPressed();
-    virtual void KeyDDownPressed();
-    virtual void KeyDLeftPressed();
-    virtual void KeyDRightPressed();
-    virtual void KeyStartPressed();
-    virtual void KeyJoyXPressed(u32);
-    virtual void KeyJoyYPressed(u32);
+      void Begin();
+  
+      virtual void FrameUpdate();
+      virtual void OnCreate();
+      virtual void KeyAPressed();
+      virtual void KeyBPressed();
+      virtual void KeyZPressed();
+      virtual void KeyDUpPressed();
+      virtual void KeyDDownPressed();
+      virtual void KeyDLeftPressed();
+      virtual void KeyDRightPressed();
+      virtual void KeyCUpPressed();
+      virtual void KeyCDownPressed();
+      virtual void KeyCLeftPressed();
+      virtual void KeyCRightPressed();
+      virtual void KeyStartPressed();
+      virtual void KeyJoyXPressed();
+      virtual void KeyJoyYPressed();
 
-    void SetScreenClear();
-    void ExitFrameLoop();
+      void SetScreenClear();
+      void ExitFrameLoop();
 
-    u32 ScreenWidth();
-    u32 ScreenHeight();
-};
-
-} // namespace LibN64
+      u32 ScreenWidth();
+      u32 ScreenHeight();
+      s32 JoyDataX();
+      s32 JoyDataY();
+  };
+} 
 
 #endif
