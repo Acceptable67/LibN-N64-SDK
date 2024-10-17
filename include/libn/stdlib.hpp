bbd383f89717778958db5f60386ece15cab5fb0c
Spencer Chase <31579132+Acceptable67@users.noreply.github.com>
2024-10-17 11:10:22 -0400

    Update stdlib.hpp
    reorganized

--- include/libn/stdlib.hpp
+++ include/libn/stdlib.hpp
@@ -1,4 +1,8 @@
-/*super basic functions to get us going up until newlib implementation*/
+/* 
+ * Super basic functions to get us going 
+ * up until newlib implementation 
+ */
+
 #ifndef LIBN_STDLIB_H
 #define LIBN_STDLIB_H
 
@@ -9,16 +13,20 @@
 
 using namespace LibN64;
 
+// String Functions
 int __strlen(const char *src);
-void *__malloc(size_t size);
-void __memcpy(char *dest, char *src, size_t len);
-void __memset(void *dest, char value, size_t size);
 char *__strdup(const char *);
 int __strncmp(const char *a, const char *b, int amt);
 int __strcmp(const char *a, const char *b);
 char __toupper(char);
 char __tolower(char);
 
+// Memory Functions
+void *__malloc(size_t size);
+void __memcpy(char *dest, const char *src, size_t len);
+void __memset(void *dest, char value, size_t size);
+
+// I/O Functions
 int printf(const std::string_view formatted_string);
 extern "C" {
     void *sbrk(int incr);
@@ -35,26 +43,41 @@ extern "C" {
     void ResetConsole();
     int printf(const char *format, ...);
     int stdvprint(const s8 *ptr);
+    
+    // Assertion Functions
     void __assert_func(const char *file, int line, const char *, const char *e);
-    void __assert_func_cpp(std::string file, u32 line, [[maybe_unused]] std::string function, std::string exp,
-                std::string reason);
+    void __assert_func_cpp(std::string file, u32 line, 
+                            [[maybe_unused]] std::string function, 
+                            std::string exp, std::string reason);
     void __assert(const char *, int, const char *);
+
+    // File Operations
     int link(char *existing, char *neww);
-    int lseek([[maybe_unused]] int file, [[maybe_unused]] int ptr, [[maybe_unused]] int dir);
-    int open([[maybe_unused]] char *file, [[maybe_unused]] int flags, [[maybe_unused]] int mode);
-    int read([[maybe_unused]] int file, [[maybe_unused]] char *ptr, [[maybe_unused]] int len);
+    int lseek([[maybe_unused]] int file, 
+               [[maybe_unused]] int ptr, 
+               [[maybe_unused]] int dir);
+    int open([[maybe_unused]] char *file, 
+              [[maybe_unused]] int flags, 
+              [[maybe_unused]] int mode);
+    int read([[maybe_unused]] int file, 
+              [[maybe_unused]] char *ptr, 
+              [[maybe_unused]] int len);
     int readlink(const char *path, char *buf, size_t bufsize);
     int stat(const char *file, struct stat *st);
     int symlink(const char *path1, const char *path2);
-    clock_t times(struct tms *buf);
     int unlink(const char *name);
-    int wait(int *status);
-    int chown(const char *path, uid_t owner, gid_t group);
-    int execve(char *name, char **argv, char **env);
-    void exit(int rc);
+
+    // Process Management
     int fork(void);
-    // int gettimeofday(struct timeval *ptimeval, void *ptimezone);
+    int execve(char *name, char **argv, char **env);
+    int wait(int *status);
+    
+    // User and Time Management
+    int chown(const char *path, uid_t owner, gid_t group);
+    clock_t times(struct tms *buf);
+    
+    // Time Functions
     int gettimeofday(struct timeval *tp, struct timezone *tz);
 }
 
-#endif
+#endif // LIBN_STDLIB_H
