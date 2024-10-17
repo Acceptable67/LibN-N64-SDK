df4cb13ebc9243f70fecd343ae15aa4fc9cf91c8
Spencer Chase <31579132+Acceptable67@users.noreply.github.com>
2024-10-17 11:11:07 -0400

    added getdeltatime

--- include/libn/timer.hpp
+++ include/libn/timer.hpp
@@ -1,47 +1,103 @@
-#ifndef LIBN_TIMER_H
-#define LIBN_TIMER_H
-
 #include <functional>
+#include <string>
+#include <libn/timer.hpp>
 #include <libn/types.hpp>
 
 namespace LibN64 {
 namespace Timer {
-auto MillisecondsSinceStartup();
-float SecondsSinceStartup();
 
-enum TimerType { ONE_SHOT, CONTINUOUS_CALL, TIMER };
+extern auto MillisecondsSinceStartup() {
+    u64 ticks = 0;
+    asm("mfc0 %0, $9\n" : "=r"(ticks));
+    return ticks / ((93750000 / 2) / 1000); // take vr4300 clock speed and divide by two
+}
 
-/*Essentially a version of multithreading.
-Specified code is called every X milliseconds the user requests it to.
-Can stop, start, reset.*/
-class LibTimer {
-  private:
-    TimerType localType;
-    bool bCalled  = false;
-    bool bStarted = false;
-    f32 time_started;
-    f32 ticks;
-    f32 execute_at;
+extern float SecondsSinceStartup() {
+    return MillisecondsSinceStartup() * 0.001f;
+}
 
-    std::function<void()> localfunc;
+LibTimer::LibTimer() {
+    this->localType = TimerType::TIMER;
+    this->execute_at = 0.0f;
+    this->lastFrameTime = MillisecondsSinceStartup(); // Initialize lastFrameTime
+}
 
-  public:
-    LibTimer();
-    LibTimer(TimerType type, float tickExecution = 0.0f);
-    float GetSecondsPassed();
-    float GetMinutesPassed();
-    float GetMsPassed();
+LibTimer::LibTimer(TimerType type, float tickExecution)
+    : localType(type), execute_at(tickExecution) {
+    this->lastFrameTime = MillisecondsSinceStartup(); // Initialize lastFrameTime
+}
 
-    /*Optional function that would be called every second
-    We could just probe the COP0 for the time and subtract from
-    that to get our value but this will do.*/
-    void Update(std::function<void()> callback = [](void){});
-    void Fetch();
-    void ResetTicks();
-    void Start();
-    void Stop();
-};
+float LibTimer::GetSecondsPassed() {
+    return ticks;
+}
+
+float LibTimer::GetMinutesPassed() {
+    return ticks / 60;
+}
+
+float LibTimer::GetMsPassed() {
+    return (ticks * 1000);
+}
+
+float LibTimer::GetDeltaTime() {
+    // Get the current time in milliseconds
+    u64 currentTime = MillisecondsSinceStartup();
+    
+    // Calculate the time difference since the last frame
+    float deltaTime = (currentTime - lastFrameTime) * 0.001f; // Convert to seconds
+
+    // Update the last frame time for the next call
+    lastFrameTime = currentTime;
+
+    return deltaTime; // Return the delta time in seconds
+}
+
+void LibTimer::Update(std::function<void()> callback) {
+    auto TimerCheckAndInc = [&](bool bSetCall) {
+        this->ticks += 0.01f;
+
+        bCalled = bSetCall;
+
+        if (localType == TimerType::ONE_SHOT && ticks >= execute_at) {
+            callback();
+            bStarted = false;
+            this->ResetTicks();
+            return;
+        } else if (localType == TimerType::CONTINUOUS_CALL && ticks >= execute_at) {
+            callback();
+            this->ResetTicks();
+        }
+    };
+
+    if (bStarted) {
+        if (((Timer::MillisecondsSinceStartup() / 10) % 2) == 0) {
+            if (!bCalled) { TimerCheckAndInc(true); }
+        } else {
+            if (bCalled) { TimerCheckAndInc(false); }
+        }
+    }
+}
+
+void LibTimer::Fetch() {
+    if(this->bStarted) {
+        u64 local_time = MillisecondsSinceStartup();
+        u64 new_time = local_time - this->time_started;
+        this->ticks = new_time;
+    }
+}
+
+void LibTimer::ResetTicks() {
+    ticks = 0;
+}
+
+void LibTimer::Start() {
+    bStarted = true;
+    this->time_started = MillisecondsSinceStartup() - 1.000f;
+}
+
+void LibTimer::Stop() {
+    bStarted = false;
+}
+
 } // namespace Timer
 } // namespace LibN64
-
-#endif
